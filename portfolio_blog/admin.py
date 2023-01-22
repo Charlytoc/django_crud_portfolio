fa815af7b269e5215c773825e45230dcaef84aa1
Charly Chacon <charlyjchaconc@gmail.com>
2023-01-21 20:19:06 -0500

    verga que peo

--- portfolio_blog/admin.py
+++ portfolio_blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import User
 # Register your models here.
+from .models import Post
+
+admin.site.register(Post)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'is_staff')
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- portfolio_blog/models.py
+++ portfolio_blog/models.py
@@ -6,6 +6,6 @@ class Post(models.Model):
     title = models.CharField(max_length=100, blank=True, default='')
     description= models.TextField(default='')
     created_at= models.DateTimeField(default=timezone.now())
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=2)
     category = models.CharField(max_length=100, blank=True, default='random')
 
